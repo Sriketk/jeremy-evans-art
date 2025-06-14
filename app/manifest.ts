d95e1ff88dc83346430e78170f66a64e99f125e1
Sriket Komali <sriketk5@gmail.com>
2025-06-14 15:13:39 -0400

    new logo and removed scraping temporarily

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,20 +1,20 @@
-import type { MetadataRoute } from 'next'
+// import type { MetadataRoute } from 'next'
  
-export default function manifest(): MetadataRoute.Manifest {
-  return {
-    name: 'Jeremy Evans Portfolio',
-    short_name: 'Jeremy Evans',
-    description: 'Website featuring Jeremy evans art work',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#fff',
-    theme_color: '#fff',
-    icons: [
-      {
-        src: '/images/logo.jpeg',
-        sizes: 'any',
-        type: 'image/x-icon',
-      },
-    ],
-  }
-}
+// export default function manifest(): MetadataRoute.Manifest {
+//   return {
+//     name: 'Jeremy Evans Portfolio',
+//     short_name: 'Jeremy Evans',
+//     description: 'Website featuring Jeremy evans art work',
+//     start_url: '/',
+//     display: 'standalone',
+//     background_color: '#fff',
+//     theme_color: '#fff',
+//     icons: [
+//       {
+//         src: '/images/logo.jpeg',
+//         sizes: 'any',
+//         type: 'image/x-icon',
+//       },
+//     ],
+//   }
+// }
--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,13 @@
-import type { MetadataRoute } from 'next';
+// import type { MetadataRoute } from 'next';
 
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/', // Only disallow private routes if they exist
-    },
-    sitemap: 'https://www.jeremyevansart.com/sitemap.xml',
-    host: 'https://www.jeremyevansart.com',
-  };
-}
+// export default function robots(): MetadataRoute.Robots {
+//   return {
+//     rules: {
+//       userAgent: '*',
+//       allow: '/',
+//       disallow: '/private/', // Only disallow private routes if they exist
+//     },
+//     sitemap: 'https://www.jeremyevansart.com/sitemap.xml',
+//     host: 'https://www.jeremyevansart.com',
+//   };
+// }
