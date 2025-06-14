ec6ae25fda4fab6222b33e9fe5bb4659c2361fa2
Sriket Komali <sriketk5@gmail.com>
2025-06-14 16:30:06 -0400

    tricking next js into not caching so we can fetch often

--- app/lib/contentful/api.tsx
+++ app/lib/contentful/api.tsx
@@ -9,66 +9,78 @@ import {
   VEHICLES_CONTENT_ID,
   WOODWORK_CONTENT_ID,
   CONTROLLERS_CONTENT_ID,
-  MISC_CONTENT_ID
+  MISC_CONTENT_ID,
 } from "./constants";
 
+// TRICKING NEXT JS INTO NOT CACHING 
 export async function getHomePageContent() {
+  await fetch("https://example.com/trigger", { cache: "no-store" });
   const client = await createContentfulClient();
   const entry = await client.getEntry(HOME_PAGE_CONTENT_ID);
   return entry.fields;
 }
 
 export async function getGalleryContent() {
+  await fetch("https://example.com/trigger", { cache: "no-store" });
   const client = await createContentfulClient();
   const entry = await client.getEntries({ content_type: GALLERY_CONTENT_ID });
   return entry.items;
 }
 
 export async function getPortraitsContent() {
+  await fetch("https://example.com/trigger", { cache: "no-store" });
   const client = await createContentfulClient();
   const entry = await client.getEntries({ content_type: PORTRAITS_CONTENT_ID });
   return entry.items;
 }
 
 export async function getShoesContent() {
+  await fetch("https://example.com/trigger", { cache: "no-store" });
   const client = await createContentfulClient();
   const entry = await client.getEntries({ content_type: SHOES_CONTENT_ID });
   return entry.items;
 }
 
 export async function getSportsContent() {
+  await fetch("https://example.com/trigger", { cache: "no-store" });
   const client = await createContentfulClient();
   const entry = await client.getEntries({ content_type: SPORTS_CONTENT_ID });
   return entry.items;
 }
 
 export async function getVehiclesConent() {
+  await fetch("https://example.com/trigger", { cache: "no-store" });
   const client = await createContentfulClient();
   const entry = await client.getEntries({ content_type: VEHICLES_CONTENT_ID });
   return entry.items;
 }
 
 export async function getWoodWorkContent() {
+  await fetch("https://example.com/trigger", { cache: "no-store" });
   const client = await createContentfulClient();
   const entry = await client.getEntries({ content_type: WOODWORK_CONTENT_ID });
   return entry.items;
 }
 
 export async function getControllersContent() {
+  await fetch("https://example.com/trigger", { cache: "no-store" });
   const client = await createContentfulClient();
-  const entry = await client.getEntries({ content_type: CONTROLLERS_CONTENT_ID });
+  const entry = await client.getEntries({
+    content_type: CONTROLLERS_CONTENT_ID,
+  });
   return entry.items;
 }
 
 export async function getMiscContent() {
+  await fetch("https://example.com/trigger", { cache: "no-store" });
   const client = await createContentfulClient();
   const entry = await client.getEntries({ content_type: MISC_CONTENT_ID });
   return entry.items;
 }
 
 export async function getAboutPageContent() {
+  await fetch("https://example.com/trigger", { cache: "no-store" });
   const client = await createContentfulClient();
   const entry = await client.getEntry(ABOUT_PAGE_ID);
   return entry.fields;
 }
-
